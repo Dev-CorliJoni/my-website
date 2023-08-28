4e779fdaf1f121a7d58fa60ddb54e3950ac78847
Dev.CorliJoni <haueis.jonathan@gmail.com>
2023-08-28 21:59:07 +0200

    update groovy

--- Build-Projekt_My-Website.groovy
+++ Build-Projekt_My-Website.groovy
@@ -10,13 +10,14 @@ pipeline {
                 script {
                     assets_path = '${env.NAS_PHOTO_PROGRAMMING_PROJECTS}\\my-website\\assets'
                     assets_dest = '${env.WORKSPACE}\\src\\'
-
-                    echo '${assets_path}'
-                    echo '${assets_dest}'
-
-                    powershell 'Copy-Item -Path “${assets_path}” -Destination “${assets_dest}” -Recurse'
-                    echo 'Executed: powershell "Copy-Item -Path “${env.NAS_PHOTO_PROGRAMMING_PROJECTS}\\my-website\\assets” -Destination “${env.WORKSPACE}\\src\\” -Recurse"'
                 }
+
+                echo '${assets_path}'
+                echo '${assets_dest}'
+
+                powershell 'Copy-Item -Path “${assets_path}” -Destination “${assets_dest}” -Recurse'
+                echo 'Executed: powershell "Copy-Item -Path “${env.NAS_PHOTO_PROGRAMMING_PROJECTS}\\my-website\\assets” -Destination “${env.WORKSPACE}\\src\\” -Recurse"'
+
 
 
                 //bat("xcopy ${env.NAS_PHOTO_PROGRAMMING_PROJECTS}\\my-website\\assets ${env.WORKSPACE} /O /X /E /H /K")
