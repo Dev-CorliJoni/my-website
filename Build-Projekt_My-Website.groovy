2a817251b23cd04319e739c47caa9adc74d2dced
Dev.CorliJoni <haueis.jonathan@gmail.com>
2023-08-28 21:23:01 +0200

    update groovy

--- Build-Projekt_My-Website.groovy
+++ Build-Projekt_My-Website.groovy
@@ -5,12 +5,12 @@ pipeline {
         timeout(time: 1, unit: 'SECONDS')
     }
     stages {
-        stage('Build') {
-            stage('Copy Dependencies') {
-                steps {
-                    bat("xcopy ${env.NAS_PHOTO_PROGRAMMING_PROJECTS}\my-website\assets ${env.WORKSPACE} /O /X /E /H /K")
-                }
+        stage('Copy Dependencies') {
+            steps {
+                bat("xcopy ${env.NAS_PHOTO_PROGRAMMING_PROJECTS}\\my-website\\assets ${env.WORKSPACE} /O /X /E /H /K")
             }
+        }
+        stage('Build') {
             steps {
                 nodejs(nodeJSInstallationName: 'NODEJS_20.5.1') {
                     sh 'npm install'
