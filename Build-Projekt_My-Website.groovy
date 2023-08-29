4e7717be19978cdab45cac91101bb85633244681
Dev.CorliJoni <55767309+Dev-CorliJoni@users.noreply.github.com>
2023-08-29 13:14:44 +0200

    Update Build-Projekt_My-Website.groovy

--- Build-Projekt_My-Website.groovy
+++ Build-Projekt_My-Website.groovy
@@ -17,7 +17,8 @@ opipeline {
                         //"xcopy .\\ " + assets_dest + " /O /X /E /H /K"
                     //}
                     script {
-                        echo '' + assets_path
+                        echo 'Path: ' + assets_path
+                        echo 'Dest: ' + assets_dest
                         sh('cp -R ' + assets_path + ' ' + assets_dest)
                         //param = 'Copy-Item -Path “' + assets_path + '” -Destination “' + assets_dest + '” -Recurse'
                     //    echo "assets_path: " + assets_path
