529075629cf2588a8b26310b1fe2c016bc31dc9c
Dev.CorliJoni <55767309+Dev-CorliJoni@users.noreply.github.com>
2023-08-29 10:41:32 +0200

    Update Build-Projekt_My-Website.groovy

--- Build-Projekt_My-Website.groovy
+++ Build-Projekt_My-Website.groovy
@@ -7,8 +7,8 @@ pipeline {
     stages {
         stage('Copy Dependencies') {
             environment {
-                assets_path = env.NAS_PHOTO_PROGRAMMING_PROJECTS + '\\my-website\\assets'
-                assets_dest = env.WORKSPACE + '/src/'
+                assets_path = "${env.NAS_PHOTO_PROGRAMMING_PROJECTS}" + '\\my-website\\assets'
+                assets_dest = "${env.WORKSPACE}" + '/src/'
                 
                 assets_url = "https://nas.home:5001/fsdownload/webapi/file_download.cgi/assets.zip"
             }
