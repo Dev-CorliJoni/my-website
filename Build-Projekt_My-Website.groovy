aca26f1bfa6ccc919a21334c78823b9b87771320
Dev.CorliJoni <55767309+Dev-CorliJoni@users.noreply.github.com>
2023-08-29 11:02:34 +0200

    Update Build-Projekt_My-Website.groovy

--- Build-Projekt_My-Website.groovy
+++ Build-Projekt_My-Website.groovy
@@ -10,7 +10,7 @@ pipeline {
                 assets_path = "${env.NAS_PHOTO_PROGRAMMING_PROJECTS}" + '\\my-website\\assets'
                 assets_dest = "${env.WORKSPACE}" + '/src/'
                 
-                assets_url = "https://nas.home:5001/fsdownload/webapi/file_download.cgi/assets.zip"
+                assets_url = "https://nas.home:5001/sharing/ieG2ml8ja"
             }
             steps {
                 options {
@@ -20,7 +20,7 @@ pipeline {
                     //param = 'Copy-Item -Path “' + assets_path + '” -Destination “' + assets_dest + '” -Recurse'
 
                     echo "Requested: " + "${assets_url}"
-                    def response = httpRequest url: "${assets_url}", acceptType: APPLICATION_ZIP, consoleLogResponseBody: true, httpMode: GET, timeout: 10
+                    def response = httpRequest url: "${assets_url}", consoleLogResponseBody: true, httpMode: GET, timeout: 10
                     //outputFile: ...
                     echo "Request successfully"
                     
